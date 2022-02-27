fe718097203ce87bd9e0a140fff9fcb02ea46a6a
nadir <nadir@LAPTOP-3HIPGMSN>
2022-02-27 16:05:39 +0300

    CompletableFuture sample added(2).

--- src/main/java/com/ndr/free/concurrency/main/SampleMain10_CompletableFuture.java
+++ src/main/java/com/ndr/free/concurrency/main/SampleMain10_CompletableFuture.java
@@ -1,14 +1,71 @@
 package com.ndr.free.concurrency.main;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class SampleMain10_CompletableFuture {
 
 	public static void main(String[] args) {
-		Future<Void> futureResultVoid = CompletableFuture.runAsync(() -> System.out.println("Hello world!!"));
 		
-		Future<String> futureResultString = CompletableFuture.supplyAsync(() -> {return "Hello world!!";});
+		runAsyncSample();
+	
+		suppyAsyncSample();
+		
+		joinSample();
+		
+		thenAppySample();
+	}
+
+	
+	private static void runAsyncSample() {
+		CompletableFuture<Void> futureResultVoid = CompletableFuture.runAsync(() -> System.out.println("Hello world!!"));
+		try {
+			futureResultVoid.get();
+		} catch (InterruptedException | ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static void suppyAsyncSample() {
+		CompletableFuture<String> futureResultString = CompletableFuture.supplyAsync(() -> {return "Hello world!!";});
+		
+		try {
+			futureResultString.get();
+		} catch (InterruptedException | ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	private static void joinSample() {
+		List<Integer> intList = List.of(1,2,3,4);
+		List<CompletableFuture<Integer>>  intFutureList = intList.stream()
+				.map(n -> CompletableFuture.supplyAsync( () -> {
+																try {
+																	TimeUnit.SECONDS.sleep(n);
+																} catch (InterruptedException e) {
+																	// TODO Auto-generated catch block
+																	e.printStackTrace();
+																} 
+																return n;}))
+				.collect(Collectors.toList());
+		
+		intList = intFutureList.stream()
+				.map(CompletableFuture::join)
+				.collect(Collectors.toList());
+	}
+	
+	
+	private static void thenAppySample() {
+		CompletableFuture<String> futureResultString = CompletableFuture.supplyAsync(() -> {return "Hello ";});
+		futureResultString.thenApply( message -> {return message + " world";} );
 		
 	}
 }
